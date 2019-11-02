baeda1ed0987962b5512da55a44deb898f0dd2e9
LaserMoai <just4steam778@gmail.com>
2019-11-03 00:50:12 +0300

    Add accessibility improvement

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+// Preserve outlines for keyboard-only users
+
+function handleFirstTab(e) {
+	if (e.keyCode === 9) {
+		document.body.classList.add("user-is-tabbing");
+		window.removeEventListener("keydown", handleFirstTab);
+	}
+}
+
+window.addEventListener("keydown", handleFirstTab);
